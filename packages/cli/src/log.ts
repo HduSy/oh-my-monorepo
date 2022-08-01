d60e361a7775887fbde0d8203d62d07102edd2ea
songyao <songyao@bilibili.com>
2022-08-02 01:29:31 +0800

    :building_construction: wip(custom): cli目录结构
    no desc

---
+++ packages/cli/src/log.ts
@@ -0,0 +1,62 @@
+// 命令行个性化打印
+import chalk from 'chalk'
+import ora, { Ora } from 'ora'
+
+export const logger = {
+  info(msg: string): void {
+    console.log(`${chalk.white.bold('[INFO]')} ${msg}`)
+  },
+  warn(...msgs: Array<string | never>): void {
+    console.log.apply(null, [`${chalk.yellow.bold('[WARN]')}`, ...msgs])
+  },
+  success(msg: string): void {
+    console.log(`${chalk.green.bold('[SUCCESS]')} ${msg}`)
+  },
+  error(msg: string | Error): void {
+    console.log(`${chalk.red.bold('[ERROR]')}`, msg)
+  },
+}
+
+export const paramError = (fieldName: string, ...msgs: string[]) => {
+  console.log(`${chalk.bgYellowBright('参数格式错误')} 字段<${fieldName}>`, ...msgs)
+}
+
+let spinnerInstance: Ora | null
+export const spinner = {
+  start(text: string): void {
+    // Start the spinner. Returns the instance. Set the current text if text is provided.
+    spinnerInstance = ora(text).start()
+  },
+  info(text: string): void {
+    // ℹ
+    if (!spinnerInstance) spinnerInstance = ora('').start()
+    spinnerInstance.info(text)
+    spinnerInstance = null
+  },
+  warn(text: string): void {
+    // ⚠
+    if (!spinnerInstance) spinnerInstance = ora('').start()
+    spinnerInstance.warn(text)
+    spinnerInstance = null
+  },
+  success(text: string): void {
+    // ✔
+    if (!spinnerInstance) {
+      spinnerInstance = ora('').start()
+    }
+    spinnerInstance.succeed(text)
+    spinnerInstance = null
+  },
+  fail(text: string): void {
+    // ✖
+    if (!spinnerInstance) {
+      spinnerInstance = ora('').start()
+    }
+    spinnerInstance.fail(text)
+    spinnerInstance = null
+  },
+  stop() {
+    // Stop and clear the spinner. Returns the instance.
+    spinnerInstance.stop()
+  },
+}
