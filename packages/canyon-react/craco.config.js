a46d60ce6435561c5829bc1ca4db2c73dac0ba85
HduSy <hdusy14032122@gmail.com>
2023-02-09 10:15:21 +0800

    feat: craco 修改react项目默认webpack配置

---
+++ packages/canyon-react/craco.config.js
@@ -0,0 +1,81 @@
+const path = require('path')
+
+const pathResolve = pathUrl => path.join(__dirname, pathUrl)
+
+const webpack = require('webpack')
+
+const { name } = require('./package.json')
+
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+
+// const CracoAntDesignPlugin = require("craco-antd");
+
+// const CracoLessPlugin = require('craco-less');
+
+/* craco.config.js */
+
+module.exports = {
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.output = {
+        ...webpackConfig.output,
+
+        // 接入微前端框架qiankun的配置,不接入微前端可以不需要
+
+        library: `${name}-[name]`,
+
+        libraryTarget: 'umd',
+
+        globalObject: 'window',
+      }
+
+      webpackConfig.resolve = {
+        ...webpackConfig.resolve,
+
+        extensions: ['.tsx', '.scss', '.js'],
+
+        fallback: {
+          ...webpackConfig.resolve?.fallback,
+
+          path: false,
+        },
+      }
+
+      return webpackConfig
+    },
+
+    alias: {
+      '@': pathResolve('src'),
+
+      '@assets': pathResolve('src/assets'),
+
+      '@components': pathResolve('src/components'),
+
+      '@pages': pathResolve('src/pages'),
+
+      '@store': pathResolve('src/store'),
+
+      '@types': pathResolve('src/types'),
+
+      '@utils': pathResolve('src/utils'),
+    },
+
+    plugins: [
+      new MonacoWebpackPlugin(),
+
+      new webpack.DefinePlugin({
+        __DEBUG__: true,
+      }),
+    ],
+  },
+
+  devServer: {
+    proxy: {
+      '/x': {
+        target: 'http://localhost:3000',
+      },
+    },
+
+    open: false,
+  },
+}
